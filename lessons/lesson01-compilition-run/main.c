d567133d50311c1a5235e0633de8c867e4e62d5c
Raman <romanshcherbich@gmail.com>
2024-11-03 14:46:22 +0400

    add the main example

---
+++ lessons/lesson01-compilition-run/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Hello, %s!\n", "Roman");
+    printf("It is floating point number: %f\n", 3.14);
+    printf("it is integer number: %d\n", 442);
+    return 0;
+}
